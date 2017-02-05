55df772412608c42cb4f1ac7b0bcac81fdba618e
ayshajagiasi <aysha.jagiasi@gmail.com>
2017-02-05 12:46:42 +0530

    ui for register page

--- app/src/main/java/com/example/user/moviesdb/RegisterActivity.java
+++ app/src/main/java/com/example/user/moviesdb/RegisterActivity.java
@@ -1,10 +1,12 @@
 package com.example.user.moviesdb;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
@@ -15,7 +17,9 @@ import android.text.LoginFilter;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.EditText;
@@ -158,6 +162,24 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     }
 
     @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if ( v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    Log.d("focus", "touchevent");
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event);
+    }
+
+    @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.register_image:
@@ -288,5 +310,13 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         startActivity(setupAccountIntent);
          }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent backPresssedIntent = new Intent(RegisterActivity.this, MainActivity.class);
+        startActivity(backPresssedIntent);
+        finish();
     }
 }
